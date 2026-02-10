8f5f212a36cdccf724da8a1e46ee5a00572473e8
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-10 12:50:00 +0000

    this time

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ lastdigit = n % 10;
 
 printf("Last digit of %d is %d", n, lastdigit);
 
-if(lastdigit > 5)
+if (lastdigit > 5)
 {
 printf(" and is greater than 5\n");
 }
-else if(lastdigit == 0)
+else if (lastdigit == 0)
 {
 printf(" and is 0\n");
 }
