201607e61e062294acb8c5ac5b105f755daccfc6
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-10 13:55:27 +1100

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,5 +8,5 @@ printf("Size of a long int: %u byte(s)\n", sizeof(long));
 printf("Size of a long long int: %u byte(s)\n", sizeof(long long));
 printf("Size of a float: %u byte(s)\n", sizeof(float));
 
-return ()0;
+return (0);
 }
