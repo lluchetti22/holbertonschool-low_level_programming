54deab49380d3546abcec2e3ac06719c764afb7d
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-11 00:35:47 +1100

    Create 3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - is it positive or negative
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+
+for (i = 65; i <= 90; i++)
+{
+putchar(i);
+}
+
+putchar('\n');
+return (0);
+}
