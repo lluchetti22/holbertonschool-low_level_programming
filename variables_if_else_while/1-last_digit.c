7fc725455395c17043dc84f76265c651f5fd9bd4
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-10 12:45:20 +0000

    this time

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-int x; 
-x = n % 10;
+int lastdigit; 
+lastdigit = n % 10;
 
-printf("Last digit of %d is %d", n, x);
+printf("Last digit of %d is %d", n, lastdigit);
 
 if(n > 5)
 {
