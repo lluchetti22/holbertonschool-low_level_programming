4d779ca38bfb2514a7a8cca11eaabb93318279f6
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-10 13:30:49 +0000

    this is it

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(void){
- for (int i = 30; i <= 35; i++) {
-        putchar(i);
-		
-    }
-    return 0;
+int i;
+for (i = 97; i <= 122; i++) {
+putchar(i);
+	
+}
+
+putchar('\n');
+return 0;
 }
