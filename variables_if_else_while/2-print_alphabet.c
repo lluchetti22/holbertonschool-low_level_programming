b86ae4dcb5989e50eecc884985d1a8568704b975
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-10 13:32:24 +0000

    this is it

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/**
+* main - is it positive or negative
+*
+* Return: 0
+*/
+
 int main(void){
 int i;
-for (i = 97; i <= 122; i++) {
+
+for (i = 97; i <= 122; i++)
+{
 putchar(i);
-	
 }
 
 putchar('\n');
