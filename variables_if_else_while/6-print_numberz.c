22fc0e89de48bc5cee0958d91e9d1109615819bb
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-11 02:38:39 +0000

    print more numbers

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/**
+* main - print numbers
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
