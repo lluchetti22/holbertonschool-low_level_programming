41a708712541d4976dbb2c72a22c6d20a05dd2f6
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-11 12:39:22 +1100

    Create 4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - is it positive or negative
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+if (i != 102 || i != 113)
+{
+putchar(i);
+}
+
+}
+
+return (0);
+}
