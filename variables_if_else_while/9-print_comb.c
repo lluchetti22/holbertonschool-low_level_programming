03884a4ffe1f9ae826f6410868875a98300ca136
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-11 03:32:40 +0000

    seperation

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print numbers
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+return (0);
+}
