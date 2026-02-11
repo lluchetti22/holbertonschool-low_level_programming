f889d2133b8c6cd69b1bfc4e931176b37d114b14
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-11 02:45:26 +0000

    it is backwards

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+* main - is it positive or negative
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+
+putchar('\n');
+return (0);
+}
