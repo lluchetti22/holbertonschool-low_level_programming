472e9f2562943039b08711451e791b300dcf7543
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-11 03:03:01 +0000

    print base16 numbers

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - print Base16
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+
+for (i = 97; i <= 102; i++)
+{
+putchar(i);
+}
+
+putchar('\n');
+return (0);
+}
