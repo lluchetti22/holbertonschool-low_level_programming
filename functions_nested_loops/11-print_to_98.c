b188dd8afbdda7cb7ad23a46c35b44ef898d0584
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-13 07:57:37 +0000

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,17 +2,17 @@
 
 /**
  * print_to_98 - check the code
- *
+ * @n: number
  * Return: Always 0.
  */
 
 void print_to_98(int n)
 {
 
-if (n < 98)
+if (n < '98')
 {
 
-while(n <= 98)
+while(n <= '98')
 {
 _putchar(n);
 _putchar(',');
@@ -22,9 +22,9 @@ n++;
 }
 
 }
-else if (n > 98)
+else if (n > '98')
 {
-while(n >= 98)
+while(n >= '98')
 {
 
 _putchar(n);
