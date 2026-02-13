bb02925eda0dc4c80622b613c1cd6c34bb1c263d
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-13 06:17:11 +0000

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isalpha - print numbers
+* @n: a int
+* Return: 0
+*/
+
+int print_sign(int n)
+{
+
+if (n > 0)
+{
+return (1);
+}
+else if (n < 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
