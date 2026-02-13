80f6d0a25b08bb7e5398cf8265adccd37a25b758
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-13 06:36:47 +0000

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - check the code
+ * @n: a thing
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+
+int lastdigit;
+lastdigit = n % 10;
+
+return (lastdigit);
+}
