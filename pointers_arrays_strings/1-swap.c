d36112a67e27a71773b77618c3dcbb517cd0c813
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-16 11:21:57 +0000

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @a: stored int
+ * @b: stored int
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+
+}
