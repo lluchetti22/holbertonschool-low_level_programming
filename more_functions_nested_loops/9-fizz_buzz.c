d2a13e71a04efe6602ef6d985daa62062a890803
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-16 06:44:27 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+int x;
+
+for (x = 0; x <= 100; x++)
+{
+if ((x % 3 && x % 5) == 0)
+{
+printf("FizzBuzz\n");
+}
+else if ((x % 3) == 0)
+{
+printf("Fizz\n");
+}
+else if ((x % 5) == 0)
+{
+printf("Buzz\n");
+}
+else
+{
+printf("%d\n",x);
+}
+}
+
+
+}
