bc0c4b1faaf8aaf40eb9aff7d64aad5ee54ab6ef
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-16 06:02:07 +0000

    diag

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,28 @@
 void print_diagonal(int n)
 {
 
-while (n > 0)
+if (n <= 0)
 {
-_putchar(' ');
-n--;
-}
-if (n > 0)
-{
-_putchar('\\');
-}
 _putchar('\n');
+}
+else
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+int j;
+
+for (j = 0; j < i; j++)
+{
+
+_putchar(' ');
+
+}
+
+_putchar('\\');
+_putchar('\n');
+
+}
+}
 }
