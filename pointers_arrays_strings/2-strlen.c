81c2704b2299f663cf2a1f2fbc7f1a4e7086f8c0
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-16 11:53:17 +0000

    count

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @s: stored char
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+
+int length;
+
+while (*s != '\0')
+{
+length++;
+*s++;
+}
+
+}
