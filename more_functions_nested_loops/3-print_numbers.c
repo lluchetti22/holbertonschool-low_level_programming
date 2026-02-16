9671d647c71e20780843e4f8b6154e1ceec3cdf4
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-16 03:19:22 +0000

    number

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * 
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(i);
+
+}
+
+_putchar('\n');
+
+}
