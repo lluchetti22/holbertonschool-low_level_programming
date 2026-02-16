ebfc19d9f4173be4601fc2d705eaa24a6dcc7f20
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-16 05:12:28 +0000

    diag

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - check the code
+ * @n: int
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+
+while (n > 0)
+{
+_putchar(' ');
+n--;
+}
+_putchar('\'');
+
+_putchar('\n');
+}
