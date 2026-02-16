7fd5efc60b79dfde9dda2f32ce41ad3f3a855346
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-16 07:39:11 +0000

    hash

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - check the code
+ * @size: int
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int x, y;
+
+for (x = 0; x < size; x++)
+{
+
+for (y = 0; y <= (size - x); y++)
+{
+_putchar(' ');
+}
+
+for (y = 0; y <= size; y++)
+{
+_putchar('#');
+}
+
+_putchar('\n');
+}
+}
+
+}
