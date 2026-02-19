b483ac10abc1346c820d5fef2f8405d9f5b50028
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-19 10:52:23 +0000

    thing

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,41 +9,43 @@
 
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int res = 0;
-	int found = 0;
+int i = 0;
+int sign = 1;
+int res = 0;
+int found = 0;
 
-	while (s[i] != '\0')
-	{
-		// Handle signs: - flips the sign, + does nothing
-		if (s[i] == '-')
-			sign *= -1;
-		
-		// If we find a digit
-		if (s[i] >= '0' && s[i] <= '1' )
-		{
-			found = 1;
-			/*
-			 * Build the number as a negative to avoid overflow 
-			 * since INT_MIN (-2147483648) has a larger absolute 
-			 * value than INT_MAX (2147483647).
-			 */
-			if (sign > 0)
-				res = res * 10 - (s[i] - '0');
-			else
-				res = res * 10 + (s[i] - '0');
+while (s[i] != '\0')
+{
 
-			// If the next character isn't a digit, stop processing
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
-		i++;
-	}
+if (s[i] == '-')
+sign *= -1;
 
-	if (found == 0)
-		return (0);
 
-	// If the accumulated sign was positive, we flip our negative result back
-	return (sign > 0 ? -res : res);
+if (s[i] >= '0' && s[i] <= '1' )
+{
+found = 1;
+
+if (sign > 0)
+{
+res = res * 10 - (s[i] - '0');
+}
+else
+{
+res = res * 10 + (s[i] - '0');
+}
+
+if (s[i + 1] < '0' || s[i + 1] > '9'){
+break;
+}
+
+}
+i++;
+}
+
+if (found == 0)
+{
+return (0);
+}
+
+return (sign > 0 ? -res : res);
 }
