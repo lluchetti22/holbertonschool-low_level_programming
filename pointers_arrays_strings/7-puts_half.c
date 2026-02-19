8b66143a8b37bb90f959874323705ffea245b26c
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-19 06:36:32 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - check the code
+ * @str: stored string
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+
+int length = 0;
+int i;
+
+while (str[length] != '\0')
+{
+length++;
+}
+
+
+if (length % 2 != 0)
+{
+length++;
+}
+
+for (i = length / 2; i < length; i++)
+{
+
+_putchar(str[i]);
+
+}
+
+}
