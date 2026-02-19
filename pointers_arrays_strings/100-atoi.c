ac63a4581d06a764a8502719493639bda9b3b9d4
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-19 22:39:42 +1100

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,33 +7,40 @@
  * Return: Always 0.
  */
 
-int myAtoi(char* s)
+int _atoi(char *s)
 {
-int sign = 1, res = 0, i = 0;
+int i, sign, found;
+unsigned int res;
 
-while (s[i] == ' ')
+i = 0;
+sign = 1;
+res = 0;
+found = 0;
+
+while (s[i] != '\0')
 {
+
+if (s[i] == '-')
+{
+sign *= -1;
+}
+
+if (s[i] >= '0' && s[i] <= '9')
+{
+found = 1;
+res = (res * 10) + (s[i] - '0');
+
+if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+{
+break;
+}
+}
 i++;
 }
 
-if (s[i] == '-' || s[i] == '+')
+if (found == 0)
 {
-
-if (s[i++] == '-')
-{
-sign = -1;
-}
-}
-
-while (s[i] >= '0' && s[i] <= '9')
-{
-
-if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[idx] - '0' > 7))
-{
-return sign == 1 ? INT_MAX : INT_MIN;
-}
-
-res = 10 * res + (s[idx++] - '0');
+return (0);
 }
 
 return (res * sign);
