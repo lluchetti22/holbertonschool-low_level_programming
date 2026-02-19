fe8500428489ac2b7dcf9175c2bf91f34e117146
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-19 05:40:58 +0000

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - check the code
+ * @s: stored string
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+
+int i;
+
+for (i = 0; str[i] != '\0'; i += 2)
+{
+
+_putchar(str[i]);
+
+}
+
+_puthcar('\n');
+
+}
