ccca2da5badfa1ea8222e38ffa9b98b3cc7a6ea9
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-20 03:41:33 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - check the code
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+char *ptr = dest;
+int i =0;
+
+while (i < n && src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+i++;
+}
+
+while (i < n)
+{
+*ptr = '\0';
+ptr++;
+i++;
+}
+
+return (dest);
+}
