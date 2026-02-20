bfb33fa21d11acc171b30cfea322067c21377e99
Lachlan <lachlan.luchetti@outlook.com>
2026-02-20 16:15:32 +1100

    string:

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - check the code
+ * @s: stored char
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+
+}
+
+return (s);
+
+}
