bbd66e64d85af20ad38b5442d6148515b6f806b1
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-20 13:10:29 +1100

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,24 +10,23 @@
 
 char *_strcat(char *dest, char *src)
 {
+    char *ptr = dest;
 
-char *ptr = dest;
 
-while (*ptr != '\0')
-{
-ptr++;
-}
+    while (*ptr != '\0')
+    {
+        ptr++;
+    }
 
-while (*src != '\0')
-{
-*ptr = *src;
-ptr++;
-src++;
-}
+    while (*src != '\0')
+    {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
 
-*ptr = '\0';
+    *ptr = '\0';
 
-return (dest);
-
+    return dest;
 }
 
