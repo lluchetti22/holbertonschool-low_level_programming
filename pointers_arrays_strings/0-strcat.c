7aa961ab95f5dae96118fab5314d050f3153c091
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-20 01:55:11 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,30 +2,31 @@
 #include <stdio.h>
 
 /**
- * _strcat - check the code
- * @dest: char
- * @src: char
- * Return: Always 0.
- */
+* _strcat - check the code
+* @dest: char
+* @src: char
+* Return: Always 0.
+*/
+
 char *_strcat(char *dest, char *src)
 {
 
-    char *ptr = dest;
+char *ptr = dest;
 
-    while (*ptr != '\0')
-    {
-        ptr++;
-    }
+while (*ptr != '\0')
+{
+ptr++;
+}
 
-    while (*src != '\0')
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
+while (*src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+}
 
-    *ptr = '\0';
+*ptr = '\0';
 
-    return (dest);
+return (dest);
 
 }
