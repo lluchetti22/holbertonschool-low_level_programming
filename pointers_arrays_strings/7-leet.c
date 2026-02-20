4d5b17fc2871b34126b96b19db2536dac8843bb7
Lachlan <lachlan.luchetti@outlook.com>
2026-02-20 17:04:26 +1100

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - check the code
+ * @s: stored char
+ * Return: Always 0.
+ */
+
+char *leet(char *s)
+{
+int i, x;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "443322007711";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+
+for (x = 0; letters[x] != '\0'; x++)
+{
+if(s[i] = letters[x])
+{
+s[i] = numbers[x];
+}
+}
+}
+
+return (s);
+
+}
