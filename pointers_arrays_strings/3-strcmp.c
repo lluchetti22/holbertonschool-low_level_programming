dd98e864800b866faa0295e59f82942f72648e26
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-20 04:01:10 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - check the code
+ * @s1: char
+ * @s2: char
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+while (s1[i] != '\0' && s1[i] == s2[i])
+{
+i++;
+}
+
+return (s1[i] - s2[i]);
+}
+
