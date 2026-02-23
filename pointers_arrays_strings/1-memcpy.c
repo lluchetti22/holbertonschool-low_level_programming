f0f6b75a971fd469e0f6438d046235675b4258d7
lluchetti22 <lachlan.luchetti@outlook.com>
2026-02-23 04:32:47 +0000

    copy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - do the code
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+unsigned int i;
+for (i = 0; i< n; i++)
+{
+dest[i] = src[i];
+}
+
+return (dest);
+
+}
