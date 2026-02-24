0da9465d1f58e1662bcb296cd2051738d054defc
Lachlan <lachlan.luchetti@outlook.com>
2026-02-24 18:50:30 +1100

    4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - do the code
+ * @s: char
+ * @accept: char
+ * Return: Nothing.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+unsigned int i, j;
+/*
+* Goes through each character in the string
+*/
+for(i = 0; s[i] != '\0'; i++)
+{
+
+/*
+* finds the first instant of the character in accept in string s
+*/
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (s);
+}
+}
+}
+
+return (NULL);
+}
