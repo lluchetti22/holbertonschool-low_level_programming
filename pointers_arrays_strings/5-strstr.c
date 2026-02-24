0f399797639bf2f8f883918f46e8bb7ca0035790
Lachlan <lachlan.luchetti@outlook.com>
2026-02-24 19:18:01 +1100

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - do the code
+ * @haystack: char
+ * @needle: char
+ * Return: Nothing.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+
+}
