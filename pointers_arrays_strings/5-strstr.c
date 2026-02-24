11448cf20d40a09ef61c57b183dd2113c7e8e275
Lachlan <lachlan.luchetti@outlook.com>
2026-02-24 21:11:35 +1100

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -18,10 +18,15 @@ for (i = 0; haystack[i] != '\0'; i++)
 
 for (j = 0; needle[j] != '\0'; j++)
 {
-while (haystack[i] == needle[j])
+
+if (haystack[i + j] != needle[j])
+{
+break;
+}
+}
+if (needle[j] == '\0')
 {
 return (&haystack[i]);
-}
 }
 }
 return (NULL);
