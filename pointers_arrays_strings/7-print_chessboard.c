9cf1478bec754e7e8bf779746164cc275459a995
Lachlan <lachlan.luchetti@outlook.com>
2026-02-24 22:01:31 +1100

    chess

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - do the code
+ * @haystack: char
+ * @needle: char
+ * Return: Nothing.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+
+unsigned int i, j;
+
+for (i = 0; i < 8; i++)
+{
+
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+
+_putchar('\n');
+
+}
+}
+
