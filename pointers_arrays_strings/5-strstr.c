43ed0967df99b04603b4bb3696beae9e90efb416
Lachlan <lachlan.luchetti@outlook.com>
2026-02-24 20:27:51 +1100

    5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,5 +11,18 @@
 char *_strstr(char *haystack, char *needle)
 {
 
+unsigned int i, j;
 
+for (i = 0; haystack[i] != '\0'; i++)
+{
+
+for (j = 0; needle[j] != '\0'; j++)
+{
+while (haystack[i] == needle[j])
+{
+return (&haystack[i]);
+}
+}
+}
+return (NULL);
 }
