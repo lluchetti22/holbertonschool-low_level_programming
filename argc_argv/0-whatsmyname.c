415b908e9f5e10cc1f2a7f12a4601f6493500a1c
Lachlan <lachlan.luchetti@outlook.com>
2026-02-25 17:31:09 +1100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - check the code
+ * @argc: int
+ * @argv: char
+ * Return: Always 0.
+ */
+
 int main(int argc, char* argv[])
 {
 printf("\n", argc);
