96e58220c1b5ab6c6cc8e79495f30acd6fe964e0
Lachlan <lachlan.luchetti@outlook.com>
2026-02-25 22:53:14 +1100

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: int
+ * @argv: char
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+int i;
+
+for (i = 0; argv[i] != '\0'; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
