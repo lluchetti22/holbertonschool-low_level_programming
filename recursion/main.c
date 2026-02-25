9011353bb9be93dcca5ecfe700e915e73cd95d67
Lachlan <lachlan.luchetti@outlook.com>
2026-02-25 14:22:59 +1100

    main

--- recursion/main.c
+++ recursion/main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
- * 
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
     return (0);
 }
