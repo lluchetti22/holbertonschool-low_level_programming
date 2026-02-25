d33ec50ecec38f3bc1afe60e2a8a84ae6f716f1f
Lachlan <lachlan.luchetti@outlook.com>
2026-02-25 14:58:08 +1100

    :

---
+++ recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - check the code
+ * @n: int
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+
+if (n == 0)
+{
+return (1);
+}
+
+if (n < 0)
+{
+return (-1);
+}
+
+return(n * factorial(n -1));
+
+}
