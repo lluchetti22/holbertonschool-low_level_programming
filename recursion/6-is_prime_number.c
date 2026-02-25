d0acda688971782e27d1d13f00ed88e2295c309e
Lachlan <lachlan.luchetti@outlook.com>
2026-02-25 16:40:00 +1100

    6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * find_prime - check the code
+ * @n: int
+ * @i: int
+ * Return: Always 0.
+ */
+
+int find_prime(int n, int i)
+{
+
+if (i * i > n)
+{
+return (1);
+}
+
+if (i % i == n)
+{
+return (0);
+}
+return (find_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - check the code
+ * @n: int
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+
+if (n <= 1)
+{
+return (0);
+}
+
+return (find_prime(n, 2));
+}
