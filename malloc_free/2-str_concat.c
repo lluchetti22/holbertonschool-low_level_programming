ea62fca84e420e7eb9d6f7fea822168a1bc96995
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 13:41:22 +1100

    2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *str_concat - do the code
+ * @s1: int
+ * @s2: char
+ * Return: Nothing.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+
+
+}
