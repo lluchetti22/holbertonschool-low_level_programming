bb6771e263b7f3e7abce30c3221f2cf35c7382e4
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 13:29:31 +1100

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,56 +1,39 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * *_strdup - do the code
  * @str: char
  * Return: Nothing.
  */
-
 char *_strdup(char *str)
 {
-
 char *copy;
-unsigned int i;
-unsigned int length = 0;
-
+unsigned int i, length = 0;
 /*
 * checking if str is null
 */
 if (str == NULL)
-{
-return (NULL);
-}
-
+    return (NULL);
 /*
 * calculating the length of the string
 */
 while (str[length] != '\0')
-{
-length++;
-} 
-
+    length++;
 /*
 * allocating memory (+1 to avoid null)
 */
 copy = malloc(sizeof(char) * (length + 1));
-
 /*
 * checks to see the memory was allocated
 */
 if (copy == NULL)
-{
-return (NULL);
-}
-
+    return (NULL);
 /*
 * goes through the string and copies each char
 */
 for (i = 0; i <= length; i++)
-{
-    copy[i] = str[i];
-}
+   copy[i] = str[i];
 
 return (copy);
 }
