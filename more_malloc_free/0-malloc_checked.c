43780c57b0dcdf098fae7e92e83e8d2e553a0b8b
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 16:35:04 +1100

    more_malloc_free/

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - do the code
+ * @b: int
+ * Return: Nothing.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+
+
+}
