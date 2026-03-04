479254dc2b25efc231404b94ca3b902b0fbb830b
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 22:49:40 +1100

    structures_typedef/

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,30 +12,21 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
 dog_t *d;
 int name_len = 0, owner_len = 0, i;
 
 if (name == NULL || owner == NULL)
-{
 return (NULL);
-}
 
 while (name[name_len])
-{
 name_len++;
-}
 
 while (owner[owner_len])
-{
 owner_len++;
-}
 
 d = malloc(sizeof(dog_t));
-if(d == NULL)
-{
+if (d == NULL)
 return (NULL);
-}
 
 d->name = malloc(name_len + 1);
 if (d->name == NULL)
@@ -44,10 +35,8 @@ free(d);
 return (NULL);
 }
 
-for(i = 0; i <= name_len; i++)
-{
+for (i = 0; i <= name_len; i++)
 d->name[i] = name[i];
-}
 
 d->owner = malloc(owner_len + 1);
 if (d->owner == NULL)
@@ -57,10 +46,8 @@ free(d);
 return (NULL);
 }
 
-for(i = 0; i<= owner_len; i++)
-{
+for (i = 0; i<= owner_len; i++)
 d->owner[i] = owner[i];
-}
 
 d->age = age;
 
