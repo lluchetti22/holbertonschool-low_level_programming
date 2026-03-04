21854f50843b6d5c96b1ca16f77b56750fa3b4bd
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 15:46:22 +1100

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,33 +15,38 @@ int **alloc_grid(int width, int height)
 int **grid;
 int x, y;
 
+// checks if width or height are <= 0
 if (width <= 0 || height <= 0)
 {
 return (NULL);
 }
 
+// Allocates memory for the rows
 grid = malloc(sizeof(int *) * height);
 if (grid == NULL)
 {
 return (NULL);
 }
 
+
 for (x = 0; x < height; x++)
 {
-grid[x] = malloc(sizeof(int) * width);
 
+//Allocates memory for each column in the row
+grid[x] = malloc(sizeof(int) * width);
 if (grid[x] == NULL)
 {
 
-for  (; x >= 0; x--)
+//Free's previously allocated rows when it fails
+for  (y = 0; y < x; y++)
 {
-free(grid[x]);
+free(grid[y]);
 }
-
 free(grid);
 return (NULL);
 }
 
+// when rows are created everything in the grid is set to 0
 for (y = 0; y < width; y++)
 {
 grid[x][y] = 0;
