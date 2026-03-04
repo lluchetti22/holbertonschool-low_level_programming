16c119338cddfc190ff888faaa73c8e67d5ae365
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 21:30:44 +1100

    structures_typedef/

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - do the code
+ * @d: int
+ * Return: Nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+
+if (d == NULL)
+{
+return;
+}
+
+printf("Name: %s\n", (d->name != NULL) ? d->name: "(nil)");
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", (d->owner != NULL) ? d->owner: "(nil)");
+}
