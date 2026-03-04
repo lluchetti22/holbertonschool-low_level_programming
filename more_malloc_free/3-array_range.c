2d4af63d4cc5bf425cfb75fc261e79d4c1397f1c
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 19:22:17 +1100

    more_malloc_free/

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -3,14 +3,41 @@
 #include <stdlib.h>
 
 /**
- * *_calloc - do the code
- * @nmemb: int
- * @size: int
+ * *array_range - do the code
+ * @min: int
+ * @max: int
  * Return: Nothing.
  */
 
 int *array_range(int min, int max)
 {
 
+int *array;
+int i, size;
 
+/* Return NULL if min is > max */
+if (min > max)
+{
+return (NULL);
+}
+
+/* calc number of elements needed */
+size = max - min + 1;
+
+/* allocate memory*/
+array = malloc(sizeof(int) * size);
+
+/* returns NULL if malloc fails */
+if (array == NULL)
+{
+return (NULL);
+}
+
+/* fills array with values from min to max */
+for (i = 0; i < size; i++)
+{
+array[i] = min++;
+}
+
+return (array);
 }
