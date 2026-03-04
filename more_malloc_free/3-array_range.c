c728c5afc34786926857f1af08a61b449eacdada
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 19:20:03 +1100

    more_malloc_free/

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - do the code
+ * @nmemb: int
+ * @size: int
+ * Return: Nothing.
+ */
+
+int *array_range(int min, int max)
+{
+
+
+}
