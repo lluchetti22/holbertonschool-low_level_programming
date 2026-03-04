1e45167b0aad3577a68b6e222d55c0ef62f8354b
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 17:41:50 +1100

    more_malloc_free/

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -11,6 +11,18 @@
 void *malloc_checked(unsigned int b)
 {
 
+void *ptr;
 
+/* Allocates memory for b */
+ptr = malloc(b);
+
+/* checks if amlloc was successful */
+if (ptr == NULL)
+{
+/*ends process with status value of 98*/
+exit(98);
+}
+
+return(ptr);
 
 }
