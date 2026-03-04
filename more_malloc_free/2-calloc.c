1f3c0c65f0a512e4924bff92f63cfc1b465e0129
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 19:03:47 +1100

    more_malloc_free/

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,6 +12,28 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
+void *ptr;
+unsigned int total;
 
+/* Return NULL if either is 0 */
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
 
+/* Calculates total memory needed*/
+total = nmemb * size;
+
+/* Allocate memory */
+ptr = malloc(total);
+
+/* Returns NULL if malloc fails */
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+memset(ptr, 0, total);
+
+return (ptr);
 }
