d64bdd951776ce6eb4e222e16a0fb9f758a74042
Lachlan <lachlan.luchetti@outlook.com>
2026-03-04 18:18:17 +1100

    more_malloc_free/

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - do the code
+ * @nmemb: int
+ * @size: int
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+
+
+}
