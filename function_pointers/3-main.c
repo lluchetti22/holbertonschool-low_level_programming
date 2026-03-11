38560e42234e2030f885a3e3c30b4e1839270879
Lachlan <lachlan.luchetti@outlook.com>
2026-03-11 22:50:30 +1100

    function_pointers/

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * main - do the code
@@ -13,6 +14,33 @@ int main(int argc, char *argv[])
 int a, b;
 int (*f)(int, int);
 
+/* checks to see if the number of arguments is exactly 4 */
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
+/* getting the operator and checking that it is correct*/
+f = get_op_func(argv[2]);
+if (f == NULL || argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
 
+/* convert to ints */
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+
+/* check if the numbers are being divided or modulo by 0*/
+if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+/* execute the operation and print results*/
+printf("%d\n", f(a, b));
+return (0);
 }
