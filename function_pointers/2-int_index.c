adc666318879ba926438eb6e39cdc843f89b0e1d
Lachlan <lachlan.luchetti@outlook.com>
2026-03-11 15:59:04 +1100

    function_pointers

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - do the code
+ * @array: int
+ * @size: int
+ * @action: int
+ * Return: Nothing.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0 || size == NULL || cmp == NULL)
+{
+return (-1);
+}
+
+
+
+for(i = 0; i < size; i++)
+{
+
+if (cmp(array[i] != 0))
+{
+return (i);
+}
+
+}
+
+return (-1);
+}
