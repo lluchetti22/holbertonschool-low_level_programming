031251236ce6f828290d4fdd548146c2e551baf6
Lachlan <lachlan.luchetti@outlook.com>
2026-03-11 12:26:11 +1100

    function_pointers/

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - do the code
+ * @d: int
+ * @name: int
+ * @age: int
+ * @owner: int
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+
+
+}
