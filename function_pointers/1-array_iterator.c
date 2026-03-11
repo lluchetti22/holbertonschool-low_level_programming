a5f344184750bd650423c2e4973452455819860b
Lachlan <lachlan.luchetti@outlook.com>
2026-03-11 14:45:26 +1100

    function_pointers

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - do the code
+ * @array: int
+ * @size: int
+ * @action: int
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+
+}
