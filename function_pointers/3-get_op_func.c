8b908c3ac7a771605a156a6252b0e6b20d5df69d
Lachlan <lachlan.luchetti@outlook.com>
2026-03-11 21:55:04 +1100

    function_pointers/

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdlib.h>
 
---
+++ function_pointers/3-main.c
@@ -0,0 +1,18 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+
+/**
+ * main - do the code
+ * 
+ * Return: Nothing.
+ */
+
+int main(int argc, char *argv[])
+{
+
+int a, b;
+int (*f)(int, int);
+
+
+
+}
