1deb9d9d76d2109ee3135699a48088520ce06d03
Lachlan <lachlan.luchetti@outlook.com>
2026-03-12 15:52:05 +1100

    variadic_functions/

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,8 +3,8 @@
 #include <stdarg.h>
 
 /**
- * main - check the code
- *
+ * sum_them_all - check the code
+ * @n: int 
  * Return: Always 0.
  */
 int sum_them_all(const unsigned int n, ...)
@@ -18,7 +18,7 @@ if (n == 0)
 return (0);
 }
 
-va_start(ap,n);
+va_start(ap, n);
 
 for (i = 0; i < n; i++)
 {
