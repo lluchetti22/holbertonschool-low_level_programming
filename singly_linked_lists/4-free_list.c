be259d62045fc8a8ee6d26b6c3dce6cf18137232
Lachlan <lachlan.luchetti@outlook.com>
2026-03-26 19:03:04 +1100

    singly_linked_lists/

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - singly linked list
+ * @head: header
+ * @str: char
+ * Return: singly linked list node structure
+ */
+
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while(head != NULL)
+{
+/* saves the next node before freeing the current one */
+temp = head->next;
+
+/* free the duplicated string */
+free(head->str);
+
+/* free the node */
+free(head);
+
+/* move to the next node */
+head = temp;
+}
+}
