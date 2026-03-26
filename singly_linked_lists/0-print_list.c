9ce03fd7cc84d3f6f523a49570f91a887838ec23
Lachlan <lachlan.luchetti@outlook.com>
2026-03-26 15:22:30 +1100

    update

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @h: string
+ * Return: singly linked list node structure
+ */
+
+size_t print_list(const list_t *h){
+
+size_t count = 0;
+
+while (h != NULL)
+{
+
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+
+h = h->next;
+
+count++;
+}
+
+return (count);
+}
