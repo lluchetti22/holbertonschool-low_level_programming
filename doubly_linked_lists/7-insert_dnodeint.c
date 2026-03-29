6ef8e02b4520ee90852b41681738d639143e93fd
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 13:18:43 +1100

    doubly_linked_lists/

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - Prints all elements of a doubly linked dlistint_t.
+ * insert_dnodeint_at_index - Prints all elements of a doubly linked dlistint_t
  * @h: A pointer to the head of the dlistint_t list.
  * @idx: A pointer to the head of the dlistint_t list.
  * @n: A pointer to the head of the dlistint_t list.
@@ -15,32 +15,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 dlistint_t *new_node, *temp = *h;
 unsigned int i = 0;
 
-/* 1. Handle insertion at the beginning (index 0) */
 if (idx == 0)
 {
 return (add_dnodeint(h, n));
 }
 
-/* 2. Traverse to the node currently at idx - 1 */
 while (temp != NULL && i < (idx - 1))
 {
 temp = temp->next;
 i++;
 }
 
-/* 3. If index is out of bounds */
 if (temp == NULL)
 {
 return (NULL);
 }
 
-/* 4. Handle insertion at the end (if temp->next is NULL) */
 if (temp->next == NULL)
 {
 return (add_dnodeint_end(h, n));
 }
 
-/* 5. Create and link new node in the middle */
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 {
