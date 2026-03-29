00c8d17b46923eb8e8b944430aa98380d212e3a6
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 13:04:37 +1100

    doubly_linked_lists/

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - Prints all elements of a doubly linked dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @index: int
+ * Return: The total number of nodes in the list.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+
+while (head != NULL)
+{
+if (i == index)
+{
+return (head);
+}
+
+head = head->next;
+i++;
+}
+
+return (NULL);
+}
