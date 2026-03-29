0dc03aa90c4fde79cace4b6d0a16ff6f2cc4525a
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 15:04:09 +1100

    file_io/

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,23 +15,17 @@ int main(int argc, char *argv[])
 int file_from, file_to;
 ssize_t n_read, n_written;
 char buffer[1024];
-
-/* 1. Validate argument count */
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-
-/* 2. Open source file (must exist and be readable) */
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-
-/* 3. Open destination file (create with rw-rw-r-- or truncate) */
 file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (file_to == -1)
 {
@@ -39,8 +33,6 @@ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 close(file_from);
 exit(99);
 }
-
-/* 4. Copy loop: read 1024 bytes at a time */
 while ((n_read = read(file_from, buffer, 1024)) > 0)
 {
 n_written = write(file_to, buffer, n_read);
@@ -50,15 +42,11 @@ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
-
-/* Check for read errors */
 if (n_read == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-
-/* 5. Close file descriptors and handle errors */
 if (close(file_from) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
@@ -69,6 +57,5 @@ if (close(file_to) == -1)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 exit(100);
 }
-
 return (0);
 }
