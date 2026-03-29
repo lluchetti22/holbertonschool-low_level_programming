37363a0dd3b6a406494eb1ba8cfbe40b8ce1d2c4
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 15:06:36 +1100

    file_io/

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,50 +12,28 @@
 
 int main(int argc, char *argv[])
 {
-int file_from, file_to;
-ssize_t n_read, n_written;
-char buffer[1024];
+int f_f, f_t, r, w;
+char buf[1024];
+
 if (argc != 3)
+dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+f_f = open(argv[1], O_RDONLY);
+if (f_f == -1)
+dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+f_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (f_t == -1)
+dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+while ((r = read(f_f, buf, 1024)) > 0)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+w = write(f_t, buf, r);
+if (w == -1 || w != r)
+dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from);
-exit(99);
-}
-while ((n_read = read(file_from, buffer, 1024)) > 0)
-{
-n_written = write(file_to, buffer, n_read);
-if (n_written == -1 || n_written != n_read)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-}
-if (n_read == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-if (close(file_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-if (close(file_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
-}
+if (r == -1)
+dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+if (close(f_f) == -1)
+dprintf(2, "Error: Can't close fd %d\n", f_f), exit(100);
+if (close(f_t) == -1)
+dprintf(2, "Error: Can't close fd %d\n", f_t), exit(100);
 return (0);
 }
