68dde9fe8150fa54a2de668dd189a42d29f4fdc8
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 13:23:32 +1100

    doubly_linked_lists/

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,44 +2,46 @@
 #include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - Prints all elements of a doubly linked dlistint_t
+ * delete_dnodeint_at_index - Prints all elements of a doubly linked dlistint_t
  * @h: A pointer to the head of the dlistint_t list.
- * @idx: A pointer to the head of the dlistint_t list.
- * @n: A pointer to the head of the dlistint_t list.
+ * @index: A pointer to the head of the dlistint_t list.
  * Return: The total number of nodes in the list.
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 
-	dlistint_t *temp = *head;
-	unsigned int i = 0;
+dlistint_t *temp = *head;
+unsigned int i = 0;
 
-	if (head == NULL || *head == NULL)
-		return (-1);
+if (head == NULL || *head == NULL)
+{
+return (-1);
+}
+while (temp != NULL && i < index)
+{
+temp = temp->next;
+i++;
+}
 
-	/* 1. Locate the node at the specified index */
-	while (temp != NULL && i < index)
-	{
-		temp = temp->next;
-		i++;
-	}
+if (temp == NULL)
+{
+return (-1);
+}
 
-	/* 2. If index is out of bounds */
-	if (temp == NULL)
-		return (-1);
+if (temp == *head)
+{
+*head = temp->next;
+if (*head != NULL)
+{
+(*head)->prev = NULL;
+}
 
-	/* 3. If deleting the head node */
-	if (temp == *head)
-	{
-		*head = temp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-	}
-	else
-	{
-		/* 4. Link the previous node to the next node */
-		temp->prev->next = temp->next;
+}
+else
+{
+/* 4. Link the previous node to the next node */
+temp->prev->next = temp->next;
 if (temp->next != NULL)
 {
 temp->next->prev = temp->prev;
