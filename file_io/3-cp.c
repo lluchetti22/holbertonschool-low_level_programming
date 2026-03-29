8aecaddc3ef2c55ca0649d269177774445434f44
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 15:13:39 +1100

    file_io/

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,44 +2,70 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
- * main - Copies the content of one file to another.
- * @argc: Number of arguments.
- * @argv: Array of argument strings.
- * Return: 0 on success, or exits with specific codes on failure.
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
-int f_f, f_t, r, w;
-char buf[1024];
+int fd_from, fd_to, rd, wr;
+char buffer[1024];
 
 if (argc != 3)
 {
-dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
 }
-f_f = open(argv[1], O_RDONLY);
-if (f_f == -1)
+
+/* Open source file */
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from < 0)
 {
-dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
-f_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (f_t == -1)
+/* Open destination file: Create, Truncate, Write-only, 0664 permissions */
+fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fd_to < 0)
 {
-dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(fd_from);
+exit(99);
 }
-while ((r = read(f_f, buf, 1024)) > 0)
+
+/* Copy loop: 1024 bytes at a time */
+while ((rd = read(fd_from, buffer, 1024)) > 0)
 {
-w = write(f_t, buf, r);
-if (w == -1 || w != r)
-dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+wr = write(fd_to, buffer, rd);
+if (wr != rd)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
 }
-if (r == -1)
-dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-if (close(f_f) == -1)
-dprintf(2, "Error: Can't close fd %d\n", f_f), exit(100);
-if (close(f_t) == -1)
-dprintf(2, "Error: Can't close fd %d\n", f_t), exit(100);
+}
+
+if (rd < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+/* Close file descriptors */
+if (close(fd_from) < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+exit(100);
+}
+if (close(fd_to) < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+exit(100);
+}
+
 return (0);
 }
