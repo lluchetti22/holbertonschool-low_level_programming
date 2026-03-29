15a1f574aaea17d4ccd874da14f13703888c7380
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 13:08:59 +1100

    doubly_linked_lists/

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 
 /**
- * get_dnodeint_at_index - Prints all elements of a doubly linked dlistint_t.
+ * sum_dlistint - Prints all elements of a doubly linked dlistint_t.
  * @head: A pointer to the head of the dlistint_t list.
- * @index: int
  * Return: The total number of nodes in the list.
  */
 
