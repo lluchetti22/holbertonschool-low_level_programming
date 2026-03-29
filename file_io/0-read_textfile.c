c99ac14c0adec1a51c648350827098beaa23cd3b
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 14:20:34 +1100

    file_io/

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,7 +12,6 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
 int file;
 ssize_t bytes_read, bytes_written;
 char *buffer;
@@ -22,14 +21,12 @@ if (filename == NULL)
 return (0);
 }
 
-/* 1. Open the file */
 file = open(filename, O_RDONLY);
 if (file == -1)
 {
 return (0);
 }
 
-/* 2. Allocate buffer memory */
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
@@ -37,7 +34,6 @@ close(file);
 return (0);
 }
 
-/* 3. Read from file */
 bytes_read = read(file, buffer, letters);
 if (bytes_read == -1)
 {
@@ -46,10 +42,8 @@ close(file);
 return (0);
 }
 
-/* 4. Write to STDOUT (file descriptor 1) */
 bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-/* 5. Cleanup and validation */
 free(buffer);
 close(file);
 
