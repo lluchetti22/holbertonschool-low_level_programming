cb20920b45d6e43499c7e6e776e8c148610794b0
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 15:12:11 +1100

    file_io/

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,13 +16,19 @@ int f_f, f_t, r, w;
 char buf[1024];
 
 if (argc != 3)
+{
 dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+}
 f_f = open(argv[1], O_RDONLY);
 if (f_f == -1)
+{
 dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+}
 f_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (f_t == -1)
+{
 dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+}
 while ((r = read(f_f, buf, 1024)) > 0)
 {
 w = write(f_t, buf, r);
