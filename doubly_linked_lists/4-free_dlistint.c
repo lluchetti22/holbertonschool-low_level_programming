54da38fc885a116801e66676daf6a2294ea3da21
Lachlan <lachlan.luchetti@outlook.com>
2026-03-29 13:01:36 +1100

    doubly_linked_lists/

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - Prints all elements of a doubly linked dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * Return: The total number of nodes in the list.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+
+dlistint_t *temp;
+
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+
+}
