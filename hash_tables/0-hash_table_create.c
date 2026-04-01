f0c65178511dbee202eb291a1144eb24ae9382d6
Lachlan <lachlan.luchetti@outlook.com>
2026-04-01 15:23:55 +1100

    hash_tables/

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - does the code
+ * @size: The size of the array
+ * Return: the code
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
