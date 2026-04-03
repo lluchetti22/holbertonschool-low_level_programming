edc3a328508ff3a12e2d0b87ee7e03059d3b842d
Lachlan <lachlan.luchetti@outlook.com>
2026-04-03 12:29:47 +1100

    hash_tables/

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -12,9 +12,9 @@ unsigned long int hash_djb2(const unsigned char *str)
 unsigned long int hash = 5381;
 int c;
 
-while (( c = *str++))
+while ((c = *str++))
 {
-hash = ((hash << 5) + hash) + c;
+hash = ((hash << 5)+ hash) + c;
 }
 
 return (hash);
