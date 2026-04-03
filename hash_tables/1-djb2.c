2cd7f19c5dccfc890c2fd0d2ca80335458ad65d9
Lachlan <lachlan.luchetti@outlook.com>
2026-04-03 12:27:39 +1100

    hash_tables/

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_djb2 - does the code
+ * @str: The size of the array
+ * Return: the code
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash = 5381;
+int c;
+
+while (( c = *str++))
+{
+hash = ((hash << 5) + hash) + c;
+}
+
+return (hash);
+}
