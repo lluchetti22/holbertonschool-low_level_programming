d105d3e7377b28ea6322f8534e65747953f63d54
Lachlan <lachlan.luchetti@outlook.com>
2026-04-07 14:52:56 +1000

    hash_tables/

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_print - does the code
+ * @ht: table
+ * Return: the code
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *node;
+int first = 1;
+
+if (ht == NULL)
+{
+return;
+}
+
+printf("{}");
+
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+
+while (node != NULL)
+{
+if (first == 0)
+{
+prinf(", ");
+}
+printf("'%s': '%s'", node->key, node->value);
+
+first = 0;
+node = node->next;
+}
+}
+
+printf("}\n");
+}
+
